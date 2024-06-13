6fab406c178c16ca8585aa03239c3a55bcb4e63c
ankitsharmax <30682419+ankitsharmax@users.noreply.github.com>
2024-06-13 22:14:14 +0530

    Decoder Block add

--- src/components/model.py
+++ src/components/model.py
@@ -162,6 +162,38 @@ class Encoder(nn.Module):
             x = layer(x,mask)
         return self.norm(x)
 
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock,
+                feed_forward_block: FeedForwardBlock,dropout:float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        # src_mask for encoder
+        # tgt_mask for decoder
+        # why src_mask and tgt_mask, we are dealing with translation task, so we have a source language and a target language
+        # decoder self attention block
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x,x,x,tgt_mask))
+        # encoder key, value pair to the cross_attention_block with src_mask
+        x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x,encoder_output,encoder_output, src_mask))
+        x = self.residual_connections[2](x, self.feed_forward_block)
+        return x
+
+# According to the paper there are many N DecoderBlocks
+class Decoder(nn.Module):
+    def __int__(self,layers: nn.Module) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
+        
 
         
 
