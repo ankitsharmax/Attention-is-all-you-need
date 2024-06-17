03920d45b5a6323215e4eed798cddcbf4decace7
ankitsharmax <30682419+ankitsharmax@users.noreply.github.com>
2024-06-17 15:48:00 +0530

    get_dataset and get_model method add

--- src/components/train.py
+++ src/components/train.py
@@ -39,3 +39,31 @@ def get_dataset(config):
   train_ds_size = int(0.9 * len(ds_raw))
   val_ds_size = len(ds_raw) - train_ds_size
   train_ds , val_ds = random_split(ds_raw,[train_ds_size,val_ds_size])
+
+  train_ds = BilingualDataset(train_ds_raw,tokenizer_src, tokenizer_tgt, config['lang_src'],config['lang_tgt'],config['seq_len'])
+  valid_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'],config['lang_tgt'],config['seq_len'])
+
+  # Check the maximum length of the sentence of  the src language and the target language
+  max_len_src = 0
+  max_len_tgt = 0
+
+  for item in ds_raw:
+    src_ids = tokenizer_src.encode(item['translation'][config['lang_src']]).ids
+    tgt_ids = tokenizer_tgt.encode(item['translation'][config['lang_tgt']]).ids
+    max_len_src = max(max_len_src,len(src_ids))
+    max_len_tgt = max(max_len_tgt,len(tgt_ids))
+
+  print(f"Maximum length of the source sentence: {max_len_src}")
+  print(f"Maximum length of the target sentence: {max_len_tgt}")
+
+  train_dataloader = DataLoader(train_ds,batch_size=config['batch_size'],shuffle=True)
+  val_dataloader = DataLoader(valid_ds, batch_size=1,shuffle=True) # validation dataset batch size is taken as 1 to go through them 1 by 1 and help in visulisation
+
+  return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
+
+# build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int, d_model: int = 512, N: int = 6, h: int = 8, dropout: float = 0.1, d_ff: int = 2048) -> Transformer:
+def get_model(config,vocab_src_len,vocab_tgt_len):
+  model = build_transformer(vocab_src_len,vocab_tgt_len, config['seq_len'],config['seq_len'],config['d_model'])
+  return model
+
+
